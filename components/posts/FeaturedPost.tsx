9bde027cca19e7370eea81126df47be2e839a74c
albugowy15 <muhammadbughowy@gmail.com>
2022-07-26 11:40:54 +0700

    fetch featured post with getStaticProps

--- components/posts/FeaturedPost.tsx
+++ components/posts/FeaturedPost.tsx
@@ -1,31 +1,21 @@
 import { Post } from "contentlayer/generated";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { AiOutlineClockCircle, AiOutlineEye } from "react-icons/ai";
 import formatDate from "utils/formatDate";
 
-function FeaturedPost({ posts }: { posts: Post[] }) {
-  const [featuredPost, setFeaturedPost] = useState<Post>();
-  useEffect(() => {
-    const post = posts.find((post) => post.isFeatured === true);
-    if (typeof post != "undefined") {
-      setFeaturedPost(post);
-    }
-    console.log(featuredPost);
-  }, []);
-
+function FeaturedPost({ post }: { post: Post }) {
   return (
-    <Link href={featuredPost ? featuredPost.url : "https"}>
-      <section className="hover:cursor-pointer flex flex-col md:flex-row md:h-[20rem] lg:h-[22rem] bg-darkSecondary w-full  overflow-hidden">
+    <Link href={post ? post.url : "https"}>
+      <section className="hover:cursor-pointer flex flex-col md:flex-row md:h-[20rem] lg:h-[22rem] bg-slate-200 dark:bg-darkSecondary w-full  overflow-hidden">
         <div className="w-full h-72 lg:w-[40%] md:w-[45%] md:h-full relative object-cover overflow-hidden">
           <Image
             src={
-              featuredPost
-                ? featuredPost.thumbnail
+              post
+                ? post.thumbnail
                 : "https://images.unsplash.com/photo-1591280063444-d3c514eb6e13?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1Nzk5NjcwNA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
             }
-            alt={featuredPost?.title}
+            alt={post?.title}
             layout="fill"
             objectFit={"cover"}
           />
@@ -33,31 +23,34 @@ function FeaturedPost({ posts }: { posts: Post[] }) {
         <div className="p-4 flex flex-col justify-between flex-1">
           <div>
             <div className="flex gap-2 flex-wrap">
-              {featuredPost?.categories?.map((category, key) => (
-                <p key={key} className="bg-blueAccent p-1 rounded-md">
+              {post?.categories?.map((category, key) => (
+                <p
+                  key={key}
+                  className="bg-blueAccent p-1 text-slate-100 rounded-md"
+                >
                   {category.toLocaleString()}
                 </p>
               ))}
             </div>
             <p className="font-bold text-2xl mt-4 lg:text-4xl lg:mt-6">
-              {featuredPost?.title}
+              {post?.title}
             </p>
-            <p className="mt-2 text-xl lg:mt-4 text-white text-opacity-80">
-              {featuredPost?.description}
+            <p className="mt-2 text-xl lg:mt-4 text-opacity-80">
+              {post?.description}
             </p>
           </div>
           <div>
             <div className="flex items-center justify-between mt-6">
               <div className="flex items-center gap-2">
-                <AiOutlineClockCircle className="text-white text-opacity-60" />
-                <p className="text-white text-sm text-opacity-60">
-                  {featuredPost ? formatDate(featuredPost) : "date"}
+                <AiOutlineClockCircle className=" text-opacity-60" />
+                <p className=" text-sm text-opacity-60">
+                  {post ? formatDate(post) : "date"}
                 </p>
               </div>
               <div className="flex items-center gap-2">
-                <AiOutlineEye className="text-white text-opacity-60" />
-                <p className="text-white text-sm text-opacity-60">
-                  {featuredPost?.readingTime?.text}
+                <AiOutlineEye className=" text-opacity-60" />
+                <p className="text-sm text-opacity-60">
+                  {post?.readingTime?.text}
                 </p>
               </div>
             </div>
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,18 +1,16 @@
 import BlogPosts from "components/posts/BlogPosts";
 import FeaturedPost from "components/posts/FeaturedPost";
-import Categories from "components/sidebar/Categories";
-import TopPicks from "components/sidebar/TopPicks";
 import { Post } from "contentlayer/generated";
-import { NextPage } from "next";
 import Head from "next/head";
 import { getAllPosts } from "utils/contents";
 
-const Blog = ({ posts }: { posts: Post[] }) => {
-  function getTopPicks() {
-    const random = [...posts].sort(() => 0.5 - Math.random());
-
-    return random.slice(0, 3);
-  }
+const Blog = ({
+  posts,
+  featuredPost,
+}: {
+  posts: Post[];
+  featuredPost: Post;
+}) => {
   return (
     <>
       <Head>
@@ -24,13 +22,13 @@ const Blog = ({ posts }: { posts: Post[] }) => {
       </Head>
       <div className="py-10" />
       <h1 className="text-center font-bold text-3xl">Blog</h1>
-      <p className="text-center py-3 mb-4 text-slate-300">
+      <p className="text-center py-3 mb-4">
         Welcome to my Blog. Here I share my thoughts related to front end
         development topics and programming in general. I&apos;ve always been a
         fan of writing, and I hope you can find something here that could help
         you in your developer career.
       </p>
-      <FeaturedPost posts={posts} />
+      <FeaturedPost post={featuredPost} />
       {/* <div className="flex flex-col lg:flex-row items-start justify-between gap-3 my-6"> */}
       <div className="py-5" />
       <BlogPosts posts={posts} />
@@ -42,8 +40,8 @@ export default Blog;
 
 export async function getStaticProps() {
   const posts = getAllPosts();
-  console.log(posts);
+  const featuredPost = posts.find((post) => post.isFeatured === true);
   return {
-    props: { posts },
+    props: { posts, featuredPost },
   };
 }
