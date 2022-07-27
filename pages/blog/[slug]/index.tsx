b0115910fae5e835573bcb052526f38dc31c10c9
albugowy15 <muhammadbughowy@gmail.com>
2022-07-27 19:26:01 +0700

    add toc

--- pages/blog/[slug]/index.tsx
+++ pages/blog/[slug]/index.tsx
@@ -9,6 +9,7 @@ import { useMDXComponent } from "next-contentlayer/hooks";
 import { getAllPosts } from "utils/contents";
 import TopPicks from "components/sidebar/TopPicks";
 import Link from "next/link";
+import TOC from "components/content/TOC";
 
 function PostDetail({
   post,
@@ -17,6 +18,7 @@ function PostDetail({
   post: Post;
   recentPosts: Post[];
 }) {
+  const srcComponents = { TOC };
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
@@ -26,48 +28,44 @@ function PostDetail({
         <meta name="description" content={post.description} />
       </Head>
       <div className="py-10" />
-      <h1 className="text-3xl font-bold">{post.title}</h1>
-      <div className="mt-4 flex justify-start gap-3 items-center text-xs">
-        <Link href="/about">
-          <p className="hover:text-blueAccent transition-colors">
-            By M. K. Bughowi
-          </p>
-        </Link>
-        <span>&#8226;</span>
-        <p>{formatDate(post)}</p>
-        <span>&#8226;</span>
-        <p className="text-yellowAccent">{post.readingTime.text}</p>
-      </div>
-      <main className="flex flex-col lg:flex-row gap-3 lg:gap-5">
-        <section className="w-full lg:w-[65%]">
-          <article className="mt-5 w-full">
-            <div className="w-full h-72 md:h-96 rounded-lg overflow-hidden object-cover relative">
-              <Image
-                src={post.thumbnail}
-                alt={post.title}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <div id="content-space" className="py-3">
-              <MDXContent />
-            </div>
-            <div className="border-y-2 mt-3">
-              <p className="py-4">
-                Categories :{" "}
-                {post.categories?.map((category, idx) => (
-                  <span className="p-1 bg-blueAccent rounded-lg mr-3" key={idx}>
-                    {category.toLocaleString()}
-                  </span>
-                ))}
-              </p>
-            </div>
-            <TopPicks posts={recentPosts} />
-          </article>
-        </section>
-        <aside className="hidden lg:block lg:flex-1 sticky h-full top-14">
-          <h2>This shoud be the table of contents</h2>
-        </aside>
+
+      <main className="w-full lg:w-[65%] mx-auto">
+        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <div className="mt-2 flex justify-start gap-3 items-center text-sm">
+          <Link href="/about">
+            <p className="hover:text-blueAccent transition-colors">
+              By M. K. Bughowi
+            </p>
+          </Link>
+          <span>&#8226;</span>
+          <p>{formatDate(post)}</p>
+          <span>&#8226;</span>
+          <p className="text-yellowAccent">{post.readingTime.text}</p>
+        </div>
+        <article className="mt-5 w-full">
+          <div className="w-full h-72 md:h-96 rounded-lg overflow-hidden object-cover relative">
+            <Image
+              src={post.thumbnail}
+              alt={post.title}
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div id="content-space" className="py-3">
+            <MDXContent components={srcComponents} />
+          </div>
+          <div className="border-y-2 mt-3">
+            <p className="py-4">
+              Categories :{" "}
+              {post.categories?.map((category, idx) => (
+                <span className="p-1 bg-blueAccent rounded-lg mr-3" key={idx}>
+                  {category.toLocaleString()}
+                </span>
+              ))}
+            </p>
+          </div>
+          <TopPicks posts={recentPosts} />
+        </article>
       </main>
     </>
   );
