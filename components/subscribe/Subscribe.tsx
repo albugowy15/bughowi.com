e4da6c58c77366c031c20ff6663e2759dc132d63
albugowy15 <muhammadbughowy@gmail.com>
2022-07-27 19:25:47 +0700

    make footer stick to bottom

--- components/subscribe/Subscribe.tsx
+++ components/subscribe/Subscribe.tsx
@@ -2,7 +2,7 @@ import SocialMedia from "./SocialMedia";
 
 const Subscribe = () => {
   return (
-    <div className="text-center mx-auto p-5 mt-8">
+    <footer className="text-center mx-auto p-5 w-full">
       <h2 className="font-bold text-2xl">Stay in the Loop</h2>
       <p className="text-lg py-4">
         Subscribe to my newsletter for all the latest update
@@ -24,13 +24,13 @@ const Subscribe = () => {
       </form>
       <p className="mt-8">Find Me!</p>
       <SocialMedia />
-      <footer>
+      <div>
         <p className="text-xs mt-8 opacity-60">
           &copy; 2022 bughowi.dev - Built with Next.js, Tailwind, Contentlayer,
           Vercel
         </p>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
