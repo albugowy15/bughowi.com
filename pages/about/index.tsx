2f423d33de9e2e44ba96534e708d16a1c7980967
albugowy15 <muhammadbughowy@gmail.com>
2022-07-28 20:44:20 +0700

    add content about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
-function index() {
+import { SiTypescript, SiNextdotjs, SiTailwindcss } from "react-icons/si";
+import { FaEnvelope, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+function AboutPage() {
   return (
     <>
       <Head>
@@ -7,8 +9,9 @@ function index() {
         <meta name="description" content="Know more about me" />
       </Head>
       <div className="py-10" />
-      <h1 className="font-bold text-3xl">About Me</h1>
-      <div className="space-y-4 py-3">
+      <h1 className="font-bold text-3xl text-center">About Me</h1>
+      <div className="space-y-2 py-3 w-full lg:w-[65%] mx-auto">
+        <h2 className="font-bold text-2xl underline">Introduction</h2>
         <p>
           Hello! &#128075; I&apos;m{" "}
           <span className="font-bold text-yellowAccent">Owi</span>. I&apos;m a
@@ -36,13 +39,106 @@ function index() {
           website to be the place where I can put in practice all the new things
           I just learn.
         </p>
+        <h2 className="font-bold text-2xl underline pt-3">Tech Stack</h2>
+        <p>
+          I&apos;m currently enjoy build website using TypeScript, Next.js, and
+          Tailwind CSS.
+        </p>
+        <h3 className="font-bold text-xl pt-2">
+          <span className="mr-2">
+            <SiTypescript className="inline" />
+          </span>
+          TypeScript
+        </h3>
+        <p>
+          TypeScript allows you to write JavaScript in a way that’s right for
+          you. It is basically a JavaScript typed superset that collates to raw
+          JavaScript. The choice of TypeScript over JavaScript make me possible
+          to remove certain anomalies earlier during the development phase and
+          secure the code before going into production.
+        </p>
+        <h3 className="font-bold text-xl pt-2">
+          <span className="mr-2">
+            <SiNextdotjs className="inline" />
+          </span>
+          Next.js
+        </h3>
+        <p>
+          Using Next.js is make me easier to handle page routing with Next.js
+          File system routing feature. The data fetching (SSG, SSR, ISR) is also
+          powerful feature that I really love to use it in Next.js.
+        </p>
+        <h3 className="font-bold text-xl pt-2">
+          <span className="mr-2">
+            <SiTailwindcss className="inline" />
+          </span>
+          Tailwind
+        </h3>
+        <p>
+          And for CSS framework, I will never switch to another CSS framework
+          because Tailwind is really awesome. And I couldn&apos;t imagine how I
+          build a website without Tailwind. But possibly I will learn another
+          Javascript framework such as Angular or Vue.js in the future.
+        </p>
+        <h2 className="font-bold text-2xl underline pt-3">Contact Me</h2>
         <p>
           If you want to contact me, go ahead and send me a message through my
-          social media below. I&apos;ll be happy to receive your feedback.
+          email and social media below. I&apos;ll be happy to receive your
+          feedback.
         </p>
+        <div className="text-lg p-4 rounded border-gray-800 w-fit flex flex-wrap justify-center gap-5">
+          <div className="flex items-center flex-start gap-1 border border-gray-800 p-2 rounded hover:scale-105 hover:cursor-pointer transition">
+            <FaEnvelope />
+            <p>
+              <a
+                href="mailto:kholidbughowi@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                kholidbughowi@gmail.com
+              </a>
+            </p>
+          </div>
+          <div className="flex items-center flex-start gap-1 border border-gray-800 p-2 rounded hover:scale-105 hover:cursor-pointer transition">
+            <FaLinkedin className="text-blue-600" />
+            <p>
+              <a
+                href="https://linkedin.com/in/bughowi/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Mohamad Kholid Bughowi
+              </a>
+            </p>
+          </div>
+          <div className="flex items-center flex-start gap-1 border border-gray-800 p-2 rounded hover:scale-105 hover:cursor-pointer transition">
+            <FaTwitter className="text-sky-500" />
+            <p>
+              <a
+                href="https://twitter.com/bughowy/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                bughowy
+              </a>
+            </p>
+          </div>
+          <div className="flex items-center flex-start gap-1 border border-gray-800 p-2 rounded hover:scale-105 hover:cursor-pointer transition">
+            <FaInstagram className="text-pink-500" />
+            <p>
+              <a
+                href="https://instagram.com/muhammad_bughowy/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                muhammad_bughowy
+              </a>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default index;
+export default AboutPage;
