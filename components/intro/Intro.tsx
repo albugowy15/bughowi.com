757a6b632b8d32ca16a6449157ba687b0228ab95
albugowy15 <muhammadbughowy@gmail.com>
2022-08-17 07:09:07 +0700

    remove scroll down text

--- components/intro/Intro.tsx
+++ components/intro/Intro.tsx
@@ -4,10 +4,10 @@ import OutlineGradientButton from "../button/OutlineGradientButton";
 
 const Intro = () => {
   return (
-    <div className="text-center py-20 flex items-center flex-col justify-between h-screen">
+    <div className="h-screen text-center flex flex-col items-center justify-center">
       <div>
         <p className="text-3xl sm:text-5xl font-black my-3">
-          Hi <span className="animate-wave">&#128075;</span> I'm{" "}
+          Hi <span>&#128075;</span> I'm{" "}
           <span className="text-blueAccent">Mohamad Kholid Bughowi</span>
         </p>
         <p className="text-3xl sm:text-5xl font-black">
@@ -23,15 +23,6 @@ const Intro = () => {
           <OutlineGradientButton url="/blog" title="Read My Posts" />
           <OutlineGradientButton url="/about" title="About Me" />
         </div>
-      </div>
-
-      <div>
-        <p className="font-bold mb-2">Scroll Down!</p>
-        <button className="animate-bounce z-0">
-          <a href="#recent-posts">
-            <MdOutlineKeyboardArrowDown size={30} />
-          </a>
-        </button>
       </div>
     </div>
   );
