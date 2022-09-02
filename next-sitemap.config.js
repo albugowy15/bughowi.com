cbdc5ccb41644e0bfa8290e9f710d1659738bc72
albugowy15 <muhammadbughowy@gmail.com>
2022-09-02 19:39:59 +0700

    add sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.bughowi.com',
+    generateRobotsTxt: true, // (optional)
+    generateIndexSitemap: false
+}
