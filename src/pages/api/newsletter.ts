40906135ee12f5a0d050034d75fa1d4f964426d9
albugowy15 <kholidbughowi@gmail.com>
2023-01-21 07:40:39 +0700

    add new subscriber to email list

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -1,27 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { email } = req.body;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email } = req.body;
 
-	if (!email) {
-		return res.status(400).json({ error: "Email is required" });
-	}
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
 
-	const result = await fetch("https://api.sendinblue.com/v3/contacts", {
-		method: "POST",
-		headers: {
-			accept: "application/json",
-			"Content-Type": "application/json",
-			"api-key": process.env.SENDINBLUE_API_KEY!,
-		},
-		body: JSON.stringify({ updateEnabled: false, email: email }),
-	});
+  const result = await fetch("https://api.sendinblue.com/v3/contacts", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+      "api-key": process.env.SENDINBLUE_API_KEY!,
+    },
+    body: JSON.stringify({ updateEnabled: false, email: email, listIds: [5] }),
+  });
 
-	const data = await result.json();
+  const data = await result.json();
 
-	if (!result.ok) {
-		return res.status(500).json({ error: data.error.email[0] });
-	}
+  if (!result.ok) {
+    return res.status(500).json({ error: data.error.email[0] });
+  }
 
-	return res.status(201).json({ error: "" });
+  return res.status(201).json({ error: "" });
 }
