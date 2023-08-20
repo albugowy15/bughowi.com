d6fc736887bf166b5554b555d60bdedec0df420d
albugowy15 <kholidbughowi@gmail.com>
2023-08-20 14:22:04 +0700

    refactor: to astro

---
+++ src/content/config.ts
@@ -0,0 +1,33 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    categories: z.array(z.string()),
+    thumbnail: z.string(),
+    date: z.date(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    thumbnail: z.string(),
+    date: z.date(),
+    tech: z.array(z.string()),
+  }),
+});
+
+const aboutCollection = defineCollection({
+  type: "content",
+})
+
+export const collections = {
+  'blog': blogCollection,
+  'projects': projectCollection,
+  'about': aboutCollection,
+};
