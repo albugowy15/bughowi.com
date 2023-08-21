6339e765ce5b43078740f28fbbd001ac6fd291b5
albugowy15 <kholidbughowi@gmail.com>
2023-08-21 11:06:34 +0700

    feat: add vercel and image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,30 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import vercelStatic from '@astrojs/vercel/static';
 import mdx from "@astrojs/mdx";
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercelStatic({
+    imageService: true,
+    analytics: true,
+  }),
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
     },
     remarkPlugins: [remarkReadingTime]
+  },
+  experimental: {
+    assets: true,
+  },
+  image: {
+    service: {
+      entrypoint:"astro/assets/services/sharp",
+    },
+    domains: ['images.unsplash.com', 'user-images.githubusercontent.com', 'avatars.githubusercontent.com', 'github.com', 'blogger.googleusercontent.com'],
   },
   site: 'https://www.bughowi.com',
   integrations: [tailwind(), mdx()]
