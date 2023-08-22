f06078ca306631a7088842d0a7380283242e5782
albugowy15 <kholidbughowi@gmail.com>
2023-08-22 17:21:18 +0700

    feat: better code snippets and package manager tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,21 @@ import vercelStatic from '@astrojs/vercel/static';
 import mdx from '@astrojs/mdx';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
+import expressiveCode from 'astro-expressive-code';
+
+/** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
+import preact from '@astrojs/preact';
+const astroExpressiveCodeOptions = {
+  // Example: Change the theme to "dracula"
+  theme: 'dracula',
+};
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercelStatic(),
   markdown: {
-    shikiConfig: {
-      theme: 'material-theme-palenight',
-    },
     remarkPlugins: [remarkReadingTime],
   },
   experimental: {
@@ -33,5 +37,12 @@ export default defineConfig({
     ],
   },
   site: 'https://www.bughowi.com',
-  integrations: [tailwind(), mdx(), sitemap(), react()],
+  integrations: [
+    tailwind(),
+    expressiveCode(astroExpressiveCodeOptions),
+    mdx(),
+    sitemap(),
+    react(),
+    preact(),
+  ],
 });
