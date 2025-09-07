f5e7a92be9ef1ca486a067333590476727757411
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2025-09-07 12:46:44 +0700

    chore: fix eslint config to include ts files

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,26 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
+  // TypeScript files configuration
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-strict"],
 ];
