753648c6440a47596685616d04bbd6ffe4765c8f
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2026-07-04 14:31:22 +0700

    chore: update deps

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -1,12 +1,10 @@
-import * as mdast from "mdast-util-to-string";
+import { toString } from "mdast-util-to-string";
 import getReadingTime from "reading-time";
 
 export function remarkReadingTime() {
   return (tree, { data }) => {
-    const textOnPage = mdast.toString(tree);
+    const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
-    // readingTime.text will give us minutes read as a friendly string,
-    // i.e. "3 min read"
     data.astro.frontmatter.minutesRead = readingTime.text;
   };
 }
